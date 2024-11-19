61205438ac8350ffd672b66e42b66bb9636e685f
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-11-19 13:07:34 +0300

    posts

--- src/main/java/com/slippery/linkedlnclnbackend/service/UserService.java
+++ src/main/java/com/slippery/linkedlnclnbackend/service/UserService.java
@@ -3,6 +3,7 @@ package com.slippery.linkedlnclnbackend.service;
 import com.slippery.linkedlnclnbackend.dto.UserDto;
 import com.slippery.linkedlnclnbackend.models.User;
 
+
 public interface UserService {
     UserDto registerUser(User userDetails);
     UserDto updateUser(User userDetails,Long id);
